bff3336fa0212e94853790b2a9a06327b5256621
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-09-25 22:29:16 +0200

    added sass support

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import App from './components/App';
+import './styles/css/index.css';
 
 const store = createStore(
   rootReducer,
