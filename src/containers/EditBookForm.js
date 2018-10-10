de92e878a16191bcd6431f5be1d6efe79f9bb12a
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-10-10 23:54:33 +0200

    updating category implemented

--- src/containers/EditBookForm.js
+++ src/containers/EditBookForm.js
@@ -8,13 +8,25 @@ class EditBookForm extends Component {
     this.state = {
       title: '',
       author: '',
-      category: '',
+      category_name: '',
       complete: ''
     };
   }
 
   renderSelectOptions() {
     return this.props.categories.map(cat => <option key={cat.id}>{cat.name}</option>);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.activeBook !== prevProps.activeBook) {
+      const { title, author, category_name, complete } = this.props.activeBook;
+      this.setState({
+        title,
+        author,
+        category_name,
+        complete
+      });
+    }
   }
   
   handleChange(e) {
@@ -36,7 +48,7 @@ class EditBookForm extends Component {
     this.setState({
       title: '',
       author: '',
-      category: '',
+      category_name: '',
       complete: ''
     });
     // close modal
@@ -51,7 +63,6 @@ class EditBookForm extends Component {
         className="edit-book-form"
         onSubmit={this.handleSubmit.bind(this)}
       >
-      <h1>TEST {this.props.activeBook.title}</h1>
         <div className="form-group">
           <label htmlFor="title">Title</label>
           <input
@@ -71,10 +82,10 @@ class EditBookForm extends Component {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="category">Category</label>
+          <label htmlFor="category_name">Category</label>
           <select 
-            name="category"
-            value={this.state.category}
+            name="category_name"
+            value={this.state.category_name}
             onChange={this.handleChange.bind(this)}
           >
             {this.renderSelectOptions()}
